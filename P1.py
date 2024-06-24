9f993d2582538e84556673a03e43e2b391a15dd1
kirisathurs1977 <sritharankirisathurs@gmail.com>
2024-06-24 12:53:04 +0530

    change some syntax

--- P1.py
+++ P1.py
@@ -1,8 +1,10 @@
-# Import module
 from PIL import Image
 
-# Load image from disk (change the path)
-im = Image.open("C:\\Users\Kirish\Desktop\CV\ML\kirish.png")
+# Load image from disk
+im = Image.open(r"C:\Users\Kirish\Desktop\CV\ML\kirish.png")
 
 # Show image
 im.show()
+
+# Print image format
+print(im.format)
